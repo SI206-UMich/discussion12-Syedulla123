9cc747db9adea2cd6a9237c2f93b1196a272a1f1
Sneha Yedulla <97745708+Syedulla123@users.noreply.github.com>
2022-11-30 17:29:29 -0500

    task 2 done

--- discussion_12.py
+++ discussion_12.py
@@ -28,11 +28,23 @@ def add_employee(filename, cur, conn):
     file_data = f.read()
     f.close()
     # THE REST IS UP TO YOU
-    pass
+    data = json.loads(file_data)
+    for item in data:
+        emp_id = item['employee_id']
+        first = item['first_name']
+        last = item['last_name']
+        hire_date = item['hire_date']
+        job_id = item['job_id']
+        salary = item['salary']
+
+        cur.execute('insert or ignore into employees (employee_id, first_name, last_name, job_id, hire_date, salary) values (?,?,?,?,?,?)', (emp_id, first, last, job_id, hire_date, salary))
+        conn.commit()
 
 # TASK 2: GET JOB AND HIRE_DATE INFORMATION
 def job_and_hire_date(cur, conn):
-    pass
+    cur.execute('select jobs.job_title, employees.hire_date from jobs join employees on jobs.job_id = employees.job_id order by employees.hire_date limit 1')
+    x = cur.fetchone()
+    return x[0]
 
 # TASK 3: IDENTIFY PROBLEMATIC SALARY DATA
 # Apply JOIN clause to match individual employees
